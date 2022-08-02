932a7519b2a89ca7cb8abc143c2062d2e92e873d
TheLastKookie <shimeab@stanford.edu>
2022-08-01 22:28:43 -0400

    Shorted lines of code through list comprehension

--- main.py
+++ main.py
@@ -22,21 +22,14 @@ while len(correct_guesses) < 50:
     answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct",
                                     prompt="Give me a state's name if you dare.").title()
     if answer_state == "Exit":
+        states_to_learn = [state for state in all_states if state not in correct_guesses]
+        states_df = pandas.DataFrame(states_to_learn)
+        states_df.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states and answer_state not in correct_guesses:
         state_row = states_data[states_data.state == answer_state]
         uncle_sam.goto(int(state_row.x), int(state_row.y))
         uncle_sam.write(arg=answer_state, font=("Arial", 10, "normal"))
         correct_guesses.append(answer_state)
         screen.update()
-
-states_to_learn_dict = {
-    "state": [],
-}
-
-for state in all_states:
-    if state not in correct_guesses:
-        states_to_learn_dict["state"].append(state)
-
-states_df = pandas.DataFrame(states_to_learn_dict)
-states_df.to_csv("states_to_learn.csv")
